e2b9477a5d5d73ab4a2a9034fd09cbff83bdd2eb
Luis Barreto <luis.barreto.marin@gmail.com>
2020-08-07 19:36:01 -0400

    Prueba de dibujar esmeralda y Hola mundo

--- Minecraft/core/src/game/screens/GameScreen.java
+++ Minecraft/core/src/game/screens/GameScreen.java
@@ -3,6 +3,9 @@ package game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -44,7 +47,7 @@ public class GameScreen extends BaseScreen
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
     private BiomeAssemblerClass manager;
-    
+
     //Atributos de Box2d
     private Box2DDebugRenderer debugger;
     private World world;
@@ -70,13 +73,13 @@ public class GameScreen extends BaseScreen
         super(game);
         this.player = player;
         actors = new Group();
-        
+
         //<editor-fold defaultstate="collapsed" desc="Posicionar Cámara">
         gameCam = new OrthographicCamera();
         viewport = new FillViewport(Constant.FRAME_WIDTH / Constant.PPM, Constant.FRAME_HEIGHT / Constant.PPM, gameCam);
         gameCam.position.set(Constant.FRAME_WIDTH / 2 / Constant.PPM, Constant.FRAME_HEIGHT / 2 / Constant.PPM, 0);
         gameCam.zoom += 0.2f;
-        gameCam.translate(new Vector2(2,0.3f));
+        gameCam.translate(new Vector2(2, 0.3f));
         //</editor-fold>
 
         //<editor-fold defaultstate="collapsed" desc="Construir bioma">
@@ -101,7 +104,8 @@ public class GameScreen extends BaseScreen
         return world;
     }
 
-    public Group getActors() {
+    public Group getActors()
+    {
         return actors;
     }
     //</editor-fold>
@@ -112,30 +116,29 @@ public class GameScreen extends BaseScreen
         manager = new BiomeAssemblerClass(this);
         pacificMobs = manager.getPacificMobs();
         trees = manager.getFarming();
-        
+
         for (Plant plant : trees)
         {
             actors.addActor(plant);
         }
-        
+
         for (PacificMob mob : pacificMobs)
         {
             actors.addActor(mob);
         }
-        
+
         //Instanciar actores del mundo
         this.player.create(this);
 
         //Añadir actores al grupo
         actors.addActor(player);
-        
+
         //Monstruos de prueba   
         //actors.addActor(new Skeleton(this,20,5));
-        actors.addActor(new Zombie(this,17,5));
+        actors.addActor(new Zombie(this, 17, 5));
         //actors.addActor(new Pigman(this,15,5));
         //actors.addActor(new Creeper(this,13,5));
-        
-        
+
         world.setContactListener(new WorldContactListener(player));
     }
 
@@ -144,6 +147,9 @@ public class GameScreen extends BaseScreen
     {
         viewport.update(width, height);
     }
+
+    Batch batchUI = new SpriteBatch();
+    BitmapFont font = new BitmapFont();
 
     @Override
     public void render(float delta)
@@ -158,7 +164,7 @@ public class GameScreen extends BaseScreen
         world.step(delta, 6, 2);
 
         //<editor-fold defaultstate="collapsed" desc="Mover Cámara">
-        if ((player.getBody().getPosition().x > Constant.FRAME_WIDTH / 2 / Constant.PPM) 
+        if ((player.getBody().getPosition().x > Constant.FRAME_WIDTH / 2 / Constant.PPM)
                 && player.getBody().getPosition().x < (Constant.MAX_MAP - (Constant.FRAME_WIDTH / 2)) / Constant.PPM - 4)
         {
             gameCam.position.x = player.getBody().getPosition().x + 2;
@@ -174,6 +180,12 @@ public class GameScreen extends BaseScreen
         game.getBatch().begin();
         actors.draw(game.getBatch());
         game.getBatch().end();
+
+        //Con batch aparte dibujar las cosas de la UI
+        batchUI.begin();
+        batchUI.draw(this.getAtlas().findRegion("esmeralda"), 100, 100);
+        font.draw(batchUI, "Hello World!", 100, 100);
+        batchUI.end();
     }
 
     @Override
